233a5809507a4187634a760ad7c0ba8051a95a4f
Mungert69 <contact@mahadeva.co.uk>
2023-11-23 20:50:17 +0100

    added sha3 hash algo

--- Data/HashHelper.cs
+++ Data/HashHelper.cs
@@ -22,4 +22,22 @@ public static class HashHelper
             return builder.ToString();
         }
     }
+
+      public static string ComputeSha3_256Hash(string rawData)
+    {
+        using (SHA256 sha3 = SHA256.Create()) // Create an instance of SHA3-256
+        {
+            // Convert the input string to a byte array and compute the hash
+            byte[] bytes = sha3.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+            // Convert the byte array to a hexadecimal string
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+    }
 }
