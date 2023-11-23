e36de17d0b5787de4af1743c093de6f86e391924
Mungert69 <contact@mahadeva.co.uk>
2023-11-23 03:24:11 +0100

    added migration for dateadded field

---
+++ Migrations/20231123021009_MonitorIPDateAdded.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkMonitorData.Migrations
+{
+    /// <inheritdoc />
+    public partial class MonitorIPDateAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateAdded",
+                table: "MonitorIPs",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateAdded",
+                table: "MonitorIPs");
+        }
+    }
+}
