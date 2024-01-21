7b343286ace600ec3bbdec91bdbac7df017bfc83
Mungert69 <contact@mahadeva.co.uk>
2024-01-21 03:26:19 +0000

    database migration

---
+++ Migrations/20240121032439_LoadServerRabbitPort.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkMonitorData.Migrations
+{
+    /// <inheritdoc />
+    public partial class LoadServerRabbitPort : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RabbitPort",
+                table: "LoadServers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RabbitPort",
+                table: "LoadServers");
+        }
+    }
+}
