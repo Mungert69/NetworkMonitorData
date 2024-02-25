10aec3bee747c9f1afdd07db1dbc6a999f394d78
Mungert69 <contact@mahadeva.co.uk>
2024-02-25 03:23:13 +0000

    format only

--- Data/MonitorContext.cs
+++ Data/MonitorContext.cs
@@ -18,19 +18,19 @@ namespace NetworkMonitor.Data
         public DbSet<MonitorIP> MonitorIPs { get; set; }
 
         public DbSet<UserInfo> UserInfos { get; set; }
-                public DbSet<EmailInfo> EmailInfos { get; set; }
+        public DbSet<EmailInfo> EmailInfos { get; set; }
         public DbSet<LoadServer> LoadServers { get; set; }
 
-        public DbSet<StatusItem> StatusList{get;set;}
-        public DbSet<Blog> Blogs{get;set;}
+        public DbSet<StatusItem> StatusList { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
 
         public DbSet<BlogCategory> BlogCategories { get; set; }
         public DbSet<BlogPicture> BlogPictures { get; set; }
-        public DbSet<LogChatGPTObj> ChatGPTLogs {get;set;}
-                public DbSet<UserAuthInfo> UserAuthInfos { get; set; }
-                public DbSet<ProcessorObj> ProcessorObjs {get;set;}
-                public DbSet<TestUser> TestUsers {get;set;}
-                public DbSet<PredictStatus> PredictStatuses { get; set; }
+        public DbSet<LogChatGPTObj> ChatGPTLogs { get; set; }
+        public DbSet<UserAuthInfo> UserAuthInfos { get; set; }
+        public DbSet<ProcessorObj> ProcessorObjs { get; set; }
+        public DbSet<TestUser> TestUsers { get; set; }
+        public DbSet<PredictStatus> PredictStatuses { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -44,17 +44,17 @@ namespace NetworkMonitor.Data
             modelBuilder.Entity<BlogPicture>().ToTable("BlogPictures");
             modelBuilder.Entity<BlogCategory>().ToTable("BlogCategories");
             modelBuilder.Entity<PingInfo>().ToTable("PingInfos");
-                        modelBuilder.Entity<EmailInfo>().ToTable("EmailInfos");
+            modelBuilder.Entity<EmailInfo>().ToTable("EmailInfos");
             modelBuilder.Entity<StatusObj>().ToTable("StatusObjs");
-             modelBuilder.Entity<LogChatGPTObj>().ToTable("ChatGPTLogs");
-              modelBuilder.Entity<UserAuthInfo>().ToTable("UserAuthInfos");
-               modelBuilder.Entity<ProcessorObj>().ToTable("ProcessorObjs");
-                           modelBuilder.Entity<TestUser>().ToTable("TestUsers");
-                           modelBuilder.Entity<PredictStatus>().ToTable("PredictStatuses");
-                modelBuilder.Entity<PredictStatus>().OwnsOne(p => p.ChangeDetectionResult).WithOwner();
+            modelBuilder.Entity<LogChatGPTObj>().ToTable("ChatGPTLogs");
+            modelBuilder.Entity<UserAuthInfo>().ToTable("UserAuthInfos");
+            modelBuilder.Entity<ProcessorObj>().ToTable("ProcessorObjs");
+            modelBuilder.Entity<TestUser>().ToTable("TestUsers");
+            modelBuilder.Entity<PredictStatus>().ToTable("PredictStatuses");
+            modelBuilder.Entity<PredictStatus>().OwnsOne(p => p.ChangeDetectionResult).WithOwner();
             modelBuilder.Entity<PredictStatus>().OwnsOne(p => p.SpikeDetectionResult).WithOwner();
-     
+
         }
     }
-    
+
 }
