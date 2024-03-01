4d6d5e334c3b787e29c7c0446432b92c6f690a7f
Mungert69 <contact@mahadeva.co.uk>
2024-03-01 02:05:32 +0000

    added missing fields to PredictStatuses

---
+++ Migrations/20240301020314_PreditStatusesWithDownAndIsUp.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkMonitorData.Migrations
+{
+    /// <inheritdoc />
+    public partial class PreditStatusesWithDownAndIsUp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DownCount",
+                table: "PredictStatuses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsUp",
+                table: "PredictStatuses",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DownCount",
+                table: "PredictStatuses");
+
+            migrationBuilder.DropColumn(
+                name: "IsUp",
+                table: "PredictStatuses");
+        }
+    }
+}
