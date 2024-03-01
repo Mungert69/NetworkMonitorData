95d3df414c1bf3567515c7254e052c730437d428
Mungert69 <contact@mahadeva.co.uk>
2024-03-01 16:46:13 +0000

    added UserInfo Monitor and predict alert enabled

---
+++ Migrations/20240301164522_UserInfoMonitorPredictAlertEnabled.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkMonitorData.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserInfoMonitorPredictAlertEnabled : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MonitorAlertEnabled",
+                table: "UserInfos",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PredictAlertEnabled",
+                table: "UserInfos",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MonitorAlertEnabled",
+                table: "UserInfos");
+
+            migrationBuilder.DropColumn(
+                name: "PredictAlertEnabled",
+                table: "UserInfos");
+        }
+    }
+}
