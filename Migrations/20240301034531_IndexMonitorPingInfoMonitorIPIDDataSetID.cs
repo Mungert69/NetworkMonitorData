eb7a7b193fb53bf0ac8e09f1592661eb65df4d67
Mungert69 <contact@mahadeva.co.uk>
2024-03-01 03:57:04 +0000

    added indexes to MonitorPingInfo

---
+++ Migrations/20240301034531_IndexMonitorPingInfoMonitorIPIDDataSetID.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkMonitorData.Migrations
+{
+    /// <inheritdoc />
+    public partial class IndexMonitorPingInfoMonitorIPIDDataSetID : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IDX_MonitorIPID_DataSetID",
+                table: "MonitorPingInfos",
+                columns: new[] { "MonitorIPID", "DataSetID" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IDX_MonitorIPID_DataSetID",
+                table: "MonitorPingInfos");
+        }
+    }
+}
