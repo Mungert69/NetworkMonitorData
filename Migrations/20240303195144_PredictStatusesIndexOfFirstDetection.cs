b752c81542a647f47e186ba90d66479867cd77a2
Mungert69 <contact@mahadeva.co.uk>
2024-03-04 00:36:58 +0000

    index of detection added to PredictStatuses table

---
+++ Migrations/20240303195144_PredictStatusesIndexOfFirstDetection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkMonitorData.Migrations
+{
+    /// <inheritdoc />
+    public partial class PredictStatusesIndexOfFirstDetection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ChangeDetectionResult_IndexOfFirstDetection",
+                table: "PredictStatuses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SpikeDetectionResult_IndexOfFirstDetection",
+                table: "PredictStatuses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChangeDetectionResult_IndexOfFirstDetection",
+                table: "PredictStatuses");
+
+            migrationBuilder.DropColumn(
+                name: "SpikeDetectionResult_IndexOfFirstDetection",
+                table: "PredictStatuses");
+        }
+    }
+}
