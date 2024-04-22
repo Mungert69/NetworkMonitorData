e3d51e17d35a10cb7d26133995dad8e540f1654b
Mungert69 <contact@mahadeva.co.uk>
2024-04-22 16:05:46 +0200

    alert for publc agents

---
+++ Migrations/20240422140433_ProcessorObjSendAgentDownAlert.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkMonitorData.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProcessorObjSendAgentDownAlert : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SendAgentDownAlert",
+                table: "ProcessorObjs",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SendAgentDownAlert",
+                table: "ProcessorObjs");
+        }
+    }
+}
