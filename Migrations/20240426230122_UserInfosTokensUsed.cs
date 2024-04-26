2e2437ad3affb683e9210d699d224cdc7adbb847
Mungert69 <contact@mahadeva.co.uk>
2024-04-27 01:04:08 +0200

    new Tokens Used field

---
+++ Migrations/20240426230122_UserInfosTokensUsed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkMonitorData.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserInfosTokensUsed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TokensUsed",
+                table: "UserInfos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TokensUsed",
+                table: "UserInfos");
+        }
+    }
+}
