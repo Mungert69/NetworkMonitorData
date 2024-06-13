c3bb17012a6b96a52f49db17fb04e5913cbb4673
Mungert69 <contact@mahadeva.co.uk>
2024-06-13 12:21:34 +0200

    added country and region

---
+++ Migrations/20240613102124_AddCountryCodeAndRegionToLoadServer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkMonitorData.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountryCodeAndRegionToLoadServer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "LoadServers",
+                type: "varchar(2)",
+                maxLength: 2,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "LoadServers",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "LoadServers");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "LoadServers");
+        }
+    }
+}
