d8bc7900de2413db81039f7fb546c6aec79f68e9
Mungert69 <contact@mahadeva.co.uk>
2024-12-28 15:01:43 -0600

    LoadServerUShortRabbitPort

---
+++ Migrations/20241228205519_LoadServerUShortRabbitPort.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NetworkMonitorData.Migrations
+{
+    /// <inheritdoc />
+    public partial class LoadServerUShortRabbitPort : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<ushort>(
+                name: "RabbitPort",
+                table: "LoadServers",
+                type: "smallint unsigned",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RabbitPort",
+                table: "LoadServers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(ushort),
+                oldType: "smallint unsigned");
+        }
+    }
+}
